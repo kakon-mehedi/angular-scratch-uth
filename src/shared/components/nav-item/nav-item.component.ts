033e92e07dc16360f26c119e90e9d7d51448c01d
kakon-selise <kakon.mehedi@selise.ch>
2024-07-28 10:41:51 +0600

    feat: Add recursive nav

--- src/shared/components/nav-item/nav-item.component.ts
+++ src/shared/components/nav-item/nav-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 
 @Component({
 	selector: 'app-nav-item',
@@ -8,10 +8,42 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NavItemComponent implements OnInit {
 	@Input()
 	navigation: any;
-
+	readonly panelOpenState = signal(false);
 	constructor() {}
 
 	ngOnInit(): void {
 		console.log(this.navigation);
 	}
+
+	routers = [
+		{
+			title: 1,
+		},
+
+		{
+			ttitle: 2,
+			children: [
+				{
+					title: 2.1,
+				},
+				{
+					title: 2.1,
+				},
+				{
+					title: 2.3,
+				},
+				{
+					title: 2.4,
+					children: [
+						{
+							title: 2.41,
+						},
+					],
+				},
+				{
+					title: 2.5,
+				},
+			],
+		},
+	];
 }
